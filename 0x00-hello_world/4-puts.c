bb4c6fedb87889ae13be579aaad74cbcb8fe241b
ochi3ng <johnochi3ng@gmail.com>
2022-09-08 15:53:55 -0700

    task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
-#include <studio.h>
+#include "holberton.h"
 /**
- *  main-print the string in the put function
  *
- *  Description:using the main function
- *  This is program prints "Programming is like building a multilingual puzzle
- *  Return:0
- */
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return(0)
-}		
+{
+       	print_most_numbers();
+
+	    return (0);
+}
