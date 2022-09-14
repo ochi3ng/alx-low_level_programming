72b18b1634e923de184939a990ab7ee7a1f403c9
ochi3ng <johnochi3ng@gmail.com>
2022-09-14 04:12:16 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,14 @@
  */
 void print_alphabet(void)
 {
-	int count = 0;
 	char Alph;
 
 	while (count++ <= 9)
 	{
 		for (Alph = 'a'; Alph <= 'z'; Alph++)
+		{
 			_putchar(Alph);
+		}
 		_putchar('\n');
 	}
 }
