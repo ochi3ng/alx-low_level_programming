45a705557dae290db0263e6df39122942166bac7
ochi3ng <107102272+ochi3ng@users.noreply.github.com>
2022-09-27 11:24:32 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,19 @@
  * @n: number of bytes to fill
  * Return: memory area
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
+
 	int size = n; /* only accept positive sizes */
-	
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			s[i] = b;
+	}
+
+	return (s);
+}
