68bf879f086c166765c2ad7cc83d0dddf6d46d5e
ochi3ng <johnochi3ng@gmail.com>
2022-09-30 07:23:02 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the number of arguments passed to it.
  * @argc: The number of arguments supplied to the program.
-  @argv: An array of pointers to the arguments.
+ *  @argv: An array of pointers to the arguments.
  *
  *Return: Always 0.
  */
