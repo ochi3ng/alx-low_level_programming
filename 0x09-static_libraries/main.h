8b322ca9311bb79b86d44ead5b0b46590201e491
ochi3ng <johnochi3ng@gmail.com>
2022-09-30 15:36:43 -0700

    task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: main.h
- * Auth:Ochi3ng
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
